dc1ec629ab2b2141132e5832b54ccf0fc68884a6
Nainik <nainikmehta1999@gmail.com>
2022-01-20 23:59:26 +0530

    NextJs File base routing and dynamic path routing

---
+++ pages/portfolio/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProtfolioPage = () => {
+    return <div>This is Portfolio Page</div>;
+};
+
+export default ProtfolioPage;
---
+++ pages/portfolio/last.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PortfolioLastPage = () => {
+    return <div>This is a portfolio last Page</div>;
+};
+
+export default PortfolioLastPage;
